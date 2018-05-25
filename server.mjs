84679bb5887ee182d46efd9608bf2e0feb45c640
Wésley Queiroz <wesley.coder@gmail.com>
2018-05-25 11:56:51 -0300

    Fix eslint issues

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import Koa from 'koa'
 
-import { SERVER_PORT, NODE_ENV } from './env'
+import { SERVER_PORT } from './env'
 import playground from './db'
 
 export const app = new Koa()
@@ -8,7 +8,7 @@ export const app = new Koa()
 export const startServer = async () => {
   await playground.init()
   await app.listen(SERVER_PORT)
-  console.log(`Server running on port: ${SERVER_PORT} ( http://localhost:${SERVER_PORT}/ )`)
+  console.log(`Server running on port: ${SERVER_PORT} ( http://localhost:${SERVER_PORT}/ )`) // eslint-disable-line no-console
 }
 
 export default startServer
